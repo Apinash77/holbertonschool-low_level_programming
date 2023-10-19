4a6fce632dd3eaa4e15a05e4165cb71e889e15e1
Apu77 <apinash.suresh01@gmail.com>
2023-10-19 03:08:00 -0700

    0 putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,28 +1,14 @@
-/*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
-
 #include "holberton.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char letter;
 
-	return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int_putchar(char c);
