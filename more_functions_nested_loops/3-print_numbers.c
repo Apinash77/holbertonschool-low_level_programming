60ad8502fb31cc10969d9f9ac7ccf626baa07e98
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-02-26 12:30:26 +0100

    numbersspeak

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
-*Return: void
+* print_numbers - Print the numbers since 0 up to 9
+* Return: The numbers since 0 up to 9
 */
 
 void print_numbers(void)
-{
-	char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+{
+        int x;
+
+        for (x = 0; x <= 9; x++)
+        {
+        putchar(x + '0');
+        }
+        _putchar('\n');
 }
