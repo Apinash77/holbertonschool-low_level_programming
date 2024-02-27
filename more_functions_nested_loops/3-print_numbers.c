32d02e48d8a0503a2c7b8247e56e5329ae6e27f1
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-02-27 13:48:38 +0100

    3. The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
-void _putchar(char c) {
-    putchar(c);
-}
+void print_numbers(void)
+{
+    int i;
 
-void print_numbers(void) {
-    char digits[] = "0123456789\n";
-    for (int i = 0; i < 11; i++) {
-        _putchar(digits[i]);
-    }
-}
+    /* Print numbers 0 to 9 */
+    for (i = 0; i < 10; i++)
+        _putchar(i + '0');
 
-int main() {
-    print_numbers();
-    return 0;
+    /* Print newline */
+    _putchar('\n');
 }
