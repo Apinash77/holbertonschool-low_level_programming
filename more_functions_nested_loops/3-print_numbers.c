8f41521cc54bea951e941913ba70108fb1ad3632
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-02-27 13:52:07 +0100

    3. The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,17 @@
 
 void print_numbers(void)
 {
-    int i;
-
     /* Print numbers 0 to 9 */
-    for (i = 0; i < 10; i++)
-        _putchar(i + '0');
+    _putchar('0' + 0);
+    _putchar('0' + 1);
+    _putchar('0' + 2);
+    _putchar('0' + 3);
+    _putchar('0' + 4);
+    _putchar('0' + 5);
+    _putchar('0' + 6);
+    _putchar('0' + 7);
+    _putchar('0' + 8);
+    _putchar('0' + 9);
 
     /* Print newline */
     _putchar('\n');
