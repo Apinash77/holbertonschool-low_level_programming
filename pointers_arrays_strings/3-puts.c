31a6a0688aa747b73ee85ea3ac8d81bc1b75e0f9
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-03-07 14:32:37 +0100

    4-print_rev.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-* _puts - prints a string followed by a new line
-* @str: the string to be printed
+* _puts - Prints a string
+* @str: The string to print
 */
+
 void _puts(char *str)
 {
-	int i;
 
-	for (i = 0; str[i] != 0; i++)
-		_putchar(str[i]);
-
+        for (; *str != '\0'; str++)
+	{
+        _putchar(*str);
+        }
 	_putchar('\n');
 
 }
