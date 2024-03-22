5f13c1d55afc54a6769cf916ffd2d63d74c1e031
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-03-22 17:02:21 +0100

    1-print_rev_recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,16 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *  _print_rev_recursion - entry point
- * @s: pointer of a string with the value : 'Puts with recursion'
- * Description: function that prints a string, followed by a new line.
- * Return: 0 if success
+ * _print_rev_recursion - Print a string in revers
+ * @s: the string to reverse
+ * Return: Always 0.
  */
+
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		putchar(*s);
+		return;
 	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
