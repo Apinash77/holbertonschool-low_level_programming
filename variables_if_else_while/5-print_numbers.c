8c00c90ac57e168661fd295ebabf0c009319efbb
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-20 13:34:21 +0200

    Task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'description for betty requirement'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	char _a = '\n';
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar(_a);
+	return (0);
+}
