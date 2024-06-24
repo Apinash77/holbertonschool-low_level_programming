e986e573e4cc55c0a17b39b907567248d7936d63
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-24 14:24:43 +0200

    Task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - print : alphabet
+ * main - check the code
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+
+	_putchar('\n');
+}
