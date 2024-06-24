44703af2a43d50a9bad60725ae074e21f3fa97f4
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-24 17:42:23 +0200

    Task1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check digit
+ * @c: char to check
+ *
+ * Return: 1 for digit, 0 for non-digit
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+
+	return (0);
+}
