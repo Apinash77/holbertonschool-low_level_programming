72d36fceebd5be9691ec840e0a3e64d35cde0d1c
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-24 17:33:34 +0200

    Task0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check uppercase
+ * @c: char to check
+ *
+ * Return: 1 for uppercase, 0 for lowercase
+ */
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+
+	return (0);
+}
