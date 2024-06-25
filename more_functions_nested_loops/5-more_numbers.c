5f52ba93824587dd33e456b7cdb94f279e30b4e7
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-25 14:35:46 +0200

    TASK5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14
+ *
+ */
+void more_numbers(void)
+{
+	int i, j, second_num, limit_num = 62;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 48; i <= limit_num; i++)
+		{
+			second_num = i;
+			if (i > 57)
+			{
+				_putchar('1');
+				second_num -= 10;
+			}
+			_putchar(second_num);
+		}
+	_putchar('\n');
+	}
+}
