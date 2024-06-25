c2441a251df4b7f80f1155eec9a51d39db8d465c
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-25 14:47:24 +0200

    Task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+include "main.h"
+
+/**
+ * print_diagonal - print a line in diagonal of _
+ * @n: number of _
+ *
+ */
+void print_diagonal(int n)
+{
+	int i = 1, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i <= n)
+		{
+			_putchar('\\');
+			_putchar('\n');
+
+			if (i < n)
+			{
+				for (j = 0; j < i ; j++)
+				{
+					_putchar(' ');
+				}
+			}
+
+			i++;
+		}
+	}
+}
