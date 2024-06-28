dcf517baeccc2774e379ce2d3f57282a9f8fac93
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-28 16:19:53 +0200

    Task3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compare the first letter of 2 string
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 for equal, plus for bigger than and minus for smaller than
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result = 0, i;
+
+	for (i = 0; i < s1[i]; i++)
+	{
+		result = s1[i] - s2[i];
+
+		if (result != 0)
+		{
+			break;
+		}
+	}
+
+	return (result);
+
+}
