e12b064c4f215a4ba76d83575b2c43a35e19172b
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-28 15:18:48 +0200

    Task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * print_rev - print reverse string
- * @s: array of char
- *
- * Return: void
+ * print_rev - prints a string in reverse
+ * @s: string which reverse
+ * Return : void
  */
+
 void print_rev(char *s)
 {
-	int i = 0, j= 0;
-	char *array = s, letter;
+	int i = 0;
+	int j;
 
-	while (array[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-
-	for (j = i - 1; j >= 0; j--)
+	for (j = i - 1; j >= 0 ; j--)
 	{
-		letter = array[j];
-		_putchar(letter);
+		_putchar(s[j]);
 	}
-
 	_putchar('\n');
 }
