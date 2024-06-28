8d9c6f1e36303562cf05932de9d9043d4b152aa9
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-06-28 16:51:03 +0200

    Task5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,23 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * string_toupper- change to uppercase
+ * @str: array to cheack
+ *
+ * Return: modified array
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}
