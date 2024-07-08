d9a1ad8045667d9ca3dd8ca7f0f1d53f2ba72b9e
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-07-08 12:34:23 +0200

    Task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* main - multiply two numbers
+* @argc: count arguments
+* @argv: store arguments in string
+*
+* Return: (0)
+*/
+int main(int argc, char *argv[])
+{
+int result;
+if (argc > 2)
+{
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
+}
+else
+{
+printf("Error\n");
+}
+return (0);
+}
