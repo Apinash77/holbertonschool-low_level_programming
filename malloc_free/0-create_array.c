17527d0123ff2b1390fb4cab42bd473860385f27
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-07-10 14:54:50 +0200

    Task1

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an extensible array
+ * @size: number of element of array
+ * @c: element to insert in array
+ * Return: (s)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	/* Alloue la mémoire à s (size par la taille d'une char) */
+	s = malloc(size * sizeof(char));
+
+	/* Vérifie si l'allocation a fonctionnée */
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
+}
