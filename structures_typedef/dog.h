89c923ab7e32a6de15ffb31e3dca45770216be0c
Abinash <Abinash@MacBook-Air-de-Abisha.local>
2024-07-12 22:46:45 +0200

    Task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *struct dog - dog information
+ *@name: name of the dog
+ *@owner: owner of the dog
+ *@age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif 
